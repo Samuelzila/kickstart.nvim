d3a67861dd0862550fa512b82ec31286caeeab2e
Samuelzila <54480547+Samuelzila@users.noreply.github.com>
2025-04-30 20:36:23 -0400

    Added custom configuration
    + catppuccin
    + crates.nvim
    + lazygit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,58 @@
 --
 -- See the kickstart.nvim README for more information
 
+vim.o.wrap = true
+vim.o.linebreak = true
+
+vim.o.relativenumber = true
+
+---
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    event = 'VimEnter',
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'latte',
+      }
+      vim.cmd.colorscheme 'catppuccin'
+
+      vim.api.nvim_create_user_command('Trans', function()
+        local current_transparency = package.loaded['catppuccin'].options.transparent_background
+        require('catppuccin').setup { transparent_background = not current_transparency }
+        vim.cmd.colorscheme 'catppuccin'
+      end, {})
+    end,
+  },
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    config = function()
+      require('crates').setup()
+    end,
+  },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
