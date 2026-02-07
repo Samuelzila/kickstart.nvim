97f25d798f8ad8fba10fb42a6e63aa4be7621264
Samuelzila <54480547+Samuelzila@users.noreply.github.com>
2026-02-07 13:51:24 -0500

    Added accept word key for copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,6 +2,7 @@ vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false,
 })
+vim.keymap.set('i', '<C-L>', '<plug>(copilot-accept-word)')
 vim.g.copilot_no_tab_map = true
 
 return {
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,54 +26,10 @@ vim.g.vimtex_compiler_method = 'latexmk'
 ---@type LazySpec
 return {
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    event = 'VimEnter',
-    config = function()
-      require('catppuccin').setup {
-        flavour = 'latte',
-      }
-      vim.cmd.colorscheme 'catppuccin'
-
-      vim.api.nvim_create_user_command('Trans', function()
-        local current_transparency = package.loaded['catppuccin'].options.transparent_background
-        require('catppuccin').setup { transparent_background = not current_transparency }
-        vim.cmd.colorscheme 'catppuccin'
-      end, {})
-    end,
-  },
-  {
     'saecki/crates.nvim',
     tag = 'stable',
     config = function()
       require('crates').setup()
     end,
-  },
-  {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    },
-  },
-  { 'Civitasv/cmake-tools.nvim', opts = { terminal = { prefix_name = '[CMakeTools]: ' } } },
-  { 'github/copilot.vim' },
-  {
-    'lervag/vimtex',
-    lazy = false,
   },
 }
